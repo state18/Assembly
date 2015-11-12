199f33099a1a43d9160d3641dcedddf50e696432
Tate <state18@uncc.edu>
2015-11-12 10:41:53 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssemblyPrimes
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 2; i < input; i++)
+            {
+                var isPrime = true;
+
+                for (var j = 2; j < i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
